1f6167b30d23e8c3dd26342ded9d2ce2a0ec335d
diana.kave <diana.kave@gmail.com>
2018-02-25 15:10:33 +0200

    Excercise1 is done

--- week2/src/edu/ctco/school/exercise1/Exercise1.java
+++ week2/src/edu/ctco/school/exercise1/Exercise1.java
@@ -5,10 +5,29 @@ package edu.ctco.school.exercise1;
  *
  * How to fix it? What are the options?
  */
-private class Exercise1
+public class Exercise1
 {
-    private class SubExercise1
+    //outer classes can only be declared public or package private
+
+ class SubExercise1
     {
-        //Inner class
+        //Inner class. The main feature of an inner class is its capability to access even private members of the outer class.
     }
 }
+
+
+//i.e The main feature of an inner class is its capability to access even private members of the outer class.
+/* class Exercise1 {
+    private String pString = "I'm private";
+
+    class SubExercise1 {
+        void printPrivate() {
+            System.out.println(pString);
+        }
+    }
+
+        public static void main(String[] args) {
+            Exercise1.SubExercise1 egy = new Exercise1().new SubExercise1();
+            egy.printPrivate();
+        }
+    } */
